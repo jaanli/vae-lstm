4b02456376c46c10eb2aea2563dfe0f7cbef04fe
altosaar <jaan.altosaar@gmail.com>
2015-12-08 18:23:18 -0500

    readded run

---
+++ run.sh
@@ -0,0 +1,7 @@
+python vae_lstm.py \
+  --data_path=/Users/jaanaltosaar/projects/arxiv/dat/simple-examples/data/ \
+  --model=small \
+  --out_dir=/Users/jaanaltosaar/projects/arxiv/fit/2015-12-8-ptb-vae-lstm/
+  # --debug=True \
+  # --decode=True \
+  # --checkpoint_dir=/Users/jaanaltosaar/projects/installations/tensorflow/tensorflow/models/rnn/ptb/vae-lstm/
