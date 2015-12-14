c7e2e22d4f8d2a965e1ab2e5dad22ccb51d5b51a
Jaan Altosaar <jaan.altosaar@gmail.com>
2015-12-14 09:43:21 -0500

    sampling from same z

--- ti_run.sh
+++ ti_run.sh
@@ -4,5 +4,5 @@ module load cudatoolkit/7.0 cudann
 python vae_lstm.py \
   --data_path=/home/altosaar/projects/arxiv/dat/simple-examples/data/ \
   --model=medium \
-  --out_dir=/home/altosaar/projects/arxiv/fit/2015-12-13-ptb-vae/ \
-  # --checkpoint_dir=/home/altosaar/projects/arxiv/fit/2015-12-9-ptb-vae-lstm-small/
+  --out_dir=/home/altosaar/projects/arxiv/fit/2015-12-14-dbg/ \
+  --checkpoint_dir=/home/altosaar/projects/arxiv/fit/2015-12-13-ptb-vae-2/
