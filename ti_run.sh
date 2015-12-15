df1689d80355d27c7381f9eaf21a350634784a93
Jaan Altosaar <jaan.altosaar@gmail.com>
2015-12-15 14:54:53 -0500

    modified vae

--- ti_run.sh
+++ ti_run.sh
@@ -3,6 +3,6 @@ module load cudatoolkit/7.0 cudann
 
 python vae_lstm.py \
   --data_path=/home/altosaar/projects/arxiv/dat/simple-examples/data/ \
-  --model=medium \
-  --out_dir=/home/altosaar/projects/arxiv/fit/2015-12-14-dbg/ \
-  --checkpoint_dir=/home/altosaar/projects/arxiv/fit/2015-12-13-ptb-vae-2/
+  --model=small \
+  --out_dir=/home/altosaar/projects/arxiv/fit/2015-12-15-ptb-small/ \
+  # --checkpoint_dir=/home/altosaar/projects/arxiv/fit/2015-12-13-ptb-vae-2/
