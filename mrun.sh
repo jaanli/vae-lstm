662d9b76cacf857b8e92e5a0be4e6ee551922863
altosaar <jaan.altosaar@gmail.com>
2016-01-04 18:19:03 -0500

    mirror autoencoder working!

--- mrun.sh
+++ mrun.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 python vae_mnist.py \
-  --mirror_autoencoder \
-  --out_dir=/Users/jaanaltosaar/projects/arxiv/fit/2015-12-31-mnist-dbg/
+  --mirror_autoencoder=True \
+  --out_dir=/Users/jaanaltosaar/projects/arxiv/fit/2015-12-31-mnist-dbg/ \
+  # --nomirror_autoencoder
