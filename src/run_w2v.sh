97f95d7302cb08a3ddddf02b38ec9053f5687e98
Jaan Altosaar <jaan.altosaar@gmail.com>
2016-02-05 19:19:46 -0500

    add run word2vec on arxiv timeseries dataset!

--- src/run_w2v.sh
+++ src/run_w2v.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 DATE=`date +%Y-%m-%d`
-TENSORFLOW_BASE=/Users/jaanaltosaar/projects/installations/tensorflow
-OUT_DIR=/Users/jaanaltosaar/projects/math2vec/fit/$DATE/
+
+if [[ "$USER" == "jaan" ]];
+then
+  OUT_DIR="/home/jaan/projects/vae-lstm/fit/$DATE/"
+  TENSORFLOW_BASE=/home/jaan/installations/tensorflow
+else
+  OUT_DIR=/Users/jaanaltosaar/projects/vae-lstm/fit/$DATE/
+  TENSORFLOW_BASE=/Users/jaanaltosaar/projects/installations/tensorflow
+fi
 
 mkdir $OUT_DIR
 
-# $TENSORFLOW_BASE/bazel-bin/tensorflow/models/embedding/word2vec_optimized \
-#   --train_data=/Users/jaanaltosaar/projects/arxiv/dat/arxmliv/corpus_for_word2vec/arxmliv_no_inline_math \
-#   --eval_data=/Users/jaanaltosaar/projects/installations/tensorflow/questions-words.txt \
-#   --save_path=$OUT_DIR \
-#   --interactive=True
-
-$TENSORFLOW_BASE/word2vec_optimized \
-  --train_data=/Users/jaanaltosaar/projects/math2vec/dat/modern_small_94_thru_95_preprocessed/arxmliv_no_inline_math \
-  --eval_data=/Users/jaanaltosaar/projects/installations/tensorflow/questions-words.txt \
+$TENSORFLOW_BASE/bazel-bin/tensorflow/models/embedding/word2vec_optimized_save_hdf5 \
+  --train_data=/home/dliang/data/arxiv/dataset_2012_time_exchangeable/train.txt \
+  --eval_data=/home/jaan/projects/vae-lstm/dat/questions-words.txt \
   --save_path=$OUT_DIR \
   --interactive=True
